6329ead3f91a502540ed7756834ba8186293772a
groverjatin17 <groverjatin17@gmail.com>
2022-12-17 16:50:57 -0500

    ecommerce capstone dil

---
+++ client/src/pages/product/ProductImagesUtil.js
@@ -0,0 +1,16 @@
+import watch from "../../images/products/watch.png"
+import redtshirt from "../../images/products/tshirt_red_480x400.webp"
+import greytshirt from "../../images/products/tshirt_grey_480x400.webp"
+import blacktshirt from "../../images/products/tshirt_black_480x400.webp"
+import greentshirt from "../../images/products/tshirt_green_480x400.webp"
+import iron from "../../images/products/iron.png";
+
+
+export const productsImages = {
+    WATCH: watch,
+    REDTSHIRT: redtshirt,
+    GREYTSHIRT: greytshirt,
+    BLACKTSHIRT: blacktshirt,
+    GREENTSHIRT: greentshirt,
+    IRON: iron
+}
